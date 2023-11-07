4ef66f4a590348b1b9cf4267291a23cade4bf92d
xinling li <e1148139557@163.com>
2023-11-07 15:04:48 +0100

    debug demand with jitter

--- src/misc/helper_functions.py
+++ src/misc/helper_functions.py
@@ -7,7 +7,7 @@ def demand_update(d_ori, p, ph, pf):
     if the demand is 0 and price not 0, add jitter.
 
     d_ori: Original demand
-    p: price factor
+    p: price
     ph: maximum price
     pf: reference price.
     
@@ -15,7 +15,7 @@ def demand_update(d_ori, p, ph, pf):
     if pf == 0 and d_ori == 0:
         return 0
     elif d_ori == 0:
-        d_jitter = (ph-p*pf)/(ph-pf)
+        d_jitter = (ph-p)/(ph-pf)
         return round(d_jitter)
     else:
-        return round(d_ori*(ph-p*pf)/(ph-pf))
+        return round(d_ori*(ph-p)/(ph-pf))
